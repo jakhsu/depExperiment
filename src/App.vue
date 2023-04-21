94597aef15867d7cf5650350f3c5ae5c54f2b647
jakhsu <contactjackhsu@gmail.com>
2023-04-21 13:08:35 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { useCounterStore } from "./stores/counter.js";
+const store = useCounterStore();
+</script>
+
+<template>
+  <div>
+    <h1>experiment for dev vs regular dependency</h1>
+    {{ store.count }}
+    <button @click="store.increment()">click to increment</button>
+  </div>
+</template>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
